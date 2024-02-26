9c2f4b8c9e7efee1239e0cd89f4617a9167e383b
Muhammad Tabish <132977467+Raja-tabish@users.noreply.github.com>
2024-02-26 11:40:33 +0500

    Add files via upload

---
+++ Assignment 2/app.js
@@ -0,0 +1,81 @@
+// ..........Chapter 4 (VARIABLE NAMES: LEGAL & ILLEGAL)............
+
+// Qno1
+//  var color, legal, illegal;
+
+// Qno2
+// var firstName, lastName, $account, price123, _assign;   <<.........Legal
+// var 100rupee, @tabish, first Name, 100%, alert;            <<.........illegal
+
+// Qno3
+// (a)
+// var heading="<h2><b>Rules for naming JS variables</b></h2>";
+// document.write(heading);
+
+// // (b)
+// var example="<b>$my_1stVariable</b>";
+// document.write("Variable names can only contain number, $ and _ for example: "+example +"<br>")
+
+// // (c)
+// var name1="$name", name2="_name", name3="name";
+// document.write("Variables must begin with a letter,$ or _ For example: "+name1 + " "+ name2+" " + name3 +"</br>")
+
+// // (d)
+// var varName="sensitive";
+// document.write("Variable names are case "+varName +"<br>");
+
+// // (e)
+// var keyword="keywords";
+// document.write("Variable names should not be JS "+keyword);
+
+// ............Chapter 4 End.............
+
+// ............Chapter 5 (MATH EXPRESSIONS)..................
+
+// // Qno1
+// var num1=3; var num2=5; var sum=num1+num2;
+// document.write("Sum of " + num1 + " and " + num2 + " is " + sum + "<br>");
+
+// // Qno2
+//  // Subtraction
+//  var subtractionResult = num1 - num2;
+//  document.write("Subtraction of " + num1 + " and " + num2 + " is " + subtractionResult + "<br>");
+
+//  // Multiplication
+//  var multiplicationResult = num1 * num2;
+//  document.write("Multiplication of " + num1 + " and " + num2 + " is " + multiplicationResult + "<br>");
+
+//  // Division
+//  var divisionResult = num1 / num2;
+//  document.write("Division of " + num1 + " by " + num2 + " is " + divisionResult + "<br>");
+
+//  // Modulus
+//  var modulusResult = num1 % num2;
+//  document.write("Modulus of " + num1 + " and " + num2 + " is " + modulusResult + "<br>");
+
+// Qno3
+// // (a)
+// var value;
+// document.write("Value after variable declaration is: "+value+"<br>");
+// value="5";
+// document.write("Initial value: "+value+"<br>");
+// inc=value++;
+// document.write("Value after increment is: "+value+"<br>");
+// var value= value+7;
+// document.write("Value after addition is: "+value+"<br>");
+// dec=value--;
+// document.write("value after decrement is: "+value+"<br>");
+// var value=value%3;
+// document.write("The remainder is: "+value );
+
+// // Qno4
+// var ticketPrice=600;
+// var numberOfTicket=5;
+// var totalCost=ticketPrice * numberOfTicket;
+// document.write("Total cost to buy "+numberOfTicket+" tickets to a movie is "+totalCost+"<br>");
+
+// Qno5
+// var table=8;
+
+// document.write(table+" x 1 = "+ table + "<br>" +
+// table+" x 2 = "+ table*2 + "<br>" + table+" x 3 = " + table*3 +" <br> " + table+" x 4 = " + table*4 +" <br> " + table+" x 5 = " + table*5 +" <br> " + table+" x 6 = " + table*6 +" <br> " + table+" x 7 = " + table*7 +" <br> " + table+" x 8 = " + table*8 +" <br> " + table+" x 9 = " + table*9 +" <br> " + table+" x 10 = " + table*10 +" <br> ");
